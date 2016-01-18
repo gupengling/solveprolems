a18bf60c2ed8ce3764199fb81a53279da948bc4a
jahnny <aigupengling@163.com>
2016-01-18 10:41:41 +0800

    Initial Commit

---
+++ solveproblems/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  solveproblems
+//
+//  Created by 51offer on 16/1/18.
+//  Copyright © 2016年 Jahnny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
